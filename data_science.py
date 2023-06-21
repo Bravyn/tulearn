0f8a2fadaf2642121d91e659750ddacb8ba5eb0e
Ian Bravyn <ianbravynsa@gmail.com>
2023-06-21 09:44:24 +0300

    .

--- data_science.py
+++ data_science.py
@@ -1,12 +1,41 @@
 import streamlit as st
+import hashlib
+users = []
 
-def start_engine(key):
-    st.title("Booting Up Engine")
-    st.caption("Running on ALIEN DNA")
+def password():
+    password = st.text_input("Type in your password", type='password')
+    return password
+
+def passkey(password):
+    sha256hash = hashlib.sha256()
+    sha256hash.update(password.encode('utf-8'))
+    hash_key = sha256hash.hexdigest()
+    return hash_key
+
+def name():
     name = st.text_input("Put in your fucking username you idiot")
     if name:
-        st.write(f"For fucks sake who named you: {name}")
-        st.write("Fuking get out of here!!!")
-        st.caption("Just kidding, chill the fuck out")
+        st.write(f"For f*cks sake who named you **{name.capitalize()},** dude?")
+        st.write(":blue[Fu*king get out of here!!!]")
+ 
+        st.caption("Just kidding, chill the fuck out:sunglasses:")
+    return name
 
-start_engine()
+def save_user(name, passkey, userDB):
+    user = f"{ name } :{ passkey }"
+    if user not in userDB:
+        userDB.append(user)  
+
+
+def start_engine(key):
+    st.title("Booting Up The DS Engine")
+    st.caption("Running on ALIEN DNA")
+    
+password = password()
+passkey = passkey(password)
+name = name()
+save_user(name, passkey, users)
+
+
+
+
